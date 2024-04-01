f1d5b70f7fffc773300344bb5f951c237ac11edb
madhu <madhurasrivathsa@gmail.com>
2024-04-01 10:13:06 -0700

    1st commit

---
+++ components/footer/footer.tsx
@@ -0,0 +1,9 @@
+
+
+const Footer = () => {
+  return (
+    <div className="footer footer-center p-4 border border-1 bg-base-500 text-base-content">This is Footer</div>
+  )
+}
+
+export default Footer
